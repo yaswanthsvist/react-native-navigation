e3601bdb553ab5b3d5029eb0b827ab24c8e76927
Alexey <alexey.kureev@werkspot.nl>
2017-05-01 17:07:29 +0200

    "enable" e2e CI for Android" "

--- scripts/test.e2e.android.js
+++ scripts/test.e2e.android.js
@@ -4,11 +4,6 @@ const exec = require('shell-utils').exec;
 const release = _.includes(process.argv, 'release');
 
 function run() {
-  if (process.env.CI) {
-    console.log(`android e2e disabled on CI for now`); //eslint-disable-line
-    return;
-  }
-
   exec.execSync(`yarn run uninstall-android`);
   exec.execSync(`yarn run install-android ${release ? '-- release' : ''}`);
   exec.execSync(`cd AndroidE2E && ./gradlew connectedDebugAndroidTest`);
